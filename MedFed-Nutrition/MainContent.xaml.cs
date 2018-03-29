2db61bab215e0f7a4f1be7cbf98215fa71fa985e
Matthew Kanderka <matthew.kanderka@ucalgary.ca>
2018-03-28 21:52:08 -0600

    Converted the transitions to code so that when a button is pressed all other pages are cleared, only displaying the selected page

--- MedFed-Nutrition/MainContent.xaml.cs
+++ MedFed-Nutrition/MainContent.xaml.cs
@@ -23,7 +23,49 @@ namespace MedFed_Nutrition
         public MainContent()
         {
             InitializeComponent();
+
+            this.search_View.Visibility = Visibility.Hidden;
+            this.addControl.Visibility = Visibility.Hidden;
+            this.calorieANDNutrition.Visibility = Visibility.Hidden;
+            this.GoalsControl.Visibility = Visibility.Hidden;
+
             this.Logout.MouseLeftButtonUp += Logout_MouseLeftButtonUp;
+            this.button.Click += SearchClick;
+            this.AddRecipe.Click += AddRecipe_Click;
+            this.NutritionButton.Click += NutritionButton_Click;
+            this.Goal.Click += Goal_Click;
+        }
+
+        private void Goal_Click(object sender, RoutedEventArgs e)
+        {
+            this.search_View.Visibility = Visibility.Hidden;
+            this.addControl.Visibility = Visibility.Hidden;
+            this.calorieANDNutrition.Visibility = Visibility.Hidden;
+            this.GoalsControl.Visibility = Visibility.Visible;
+        }
+
+        private void NutritionButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.search_View.Visibility = Visibility.Hidden;
+            this.addControl.Visibility = Visibility.Hidden;
+            this.calorieANDNutrition.Visibility = Visibility.Visible;
+            this.GoalsControl.Visibility = Visibility.Hidden;
+        }
+
+        private void AddRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            this.search_View.Visibility = Visibility.Hidden;
+            this.addControl.Visibility = Visibility.Visible;
+            this.calorieANDNutrition.Visibility = Visibility.Hidden;
+            this.GoalsControl.Visibility = Visibility.Hidden;
+        }
+
+        private void SearchClick(object sender, RoutedEventArgs e)
+        {
+            this.search_View.Visibility = Visibility.Visible;
+            this.addControl.Visibility = Visibility.Hidden;
+            this.calorieANDNutrition.Visibility = Visibility.Hidden;
+            this.GoalsControl.Visibility = Visibility.Hidden;
         }
 
         private void Logout_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
